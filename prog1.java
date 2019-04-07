6e6c8bee58d94932d26707bf99d17edde8f6e083
nehashaikh06 <49342199+nehashaikh06@users.noreply.github.com>
2019-04-07 22:24:05 +0500

    Add files via upload

---
+++ prog1.java
@@ -0,0 +1,19 @@
+class prog1
+{
+public static void main(String[]args)
+{
+   String name="Neha";
+   int marks=98;
+   double per=97.2;
+   char grade='A';
+   boolean status=true;
+   char p='%';          
+ 
+ System.out.println("Name:"+ name);
+ System.out.println("Total marks:"+ marks);
+ System.out.println("Percentage:"+ per + p);
+ System.out.println("Grade:"+ grade);
+
+ System.out.println("Status:pass="+status);
+}
+}
